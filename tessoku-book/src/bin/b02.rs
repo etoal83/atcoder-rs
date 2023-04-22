75cbf18faddeec3b652c50265d6c68deea7743bf
etoal83 <etoal83@gmail.com>
2023-04-23 00:55:00 +0900

    Solve b02

--- tessoku-book/src/bin/b02.rs
+++ tessoku-book/src/bin/b02.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input!{
+        a: u32,
+        b: u32,
+    };
+
+    match (a..=b).into_iter().any(|i| 100 % i == 0) {
+        true => println!("Yes"),
+        _ => println!("No"),
+    };
 }
