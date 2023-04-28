2d0adb9cc9c05203d36037461e6905fb4bde6166
etoal83 <etoal83@gmail.com>
2023-04-28 09:54:39 +0900

    Solve b03

--- tessoku-book/src/bin/b03.rs
+++ tessoku-book/src/bin/b03.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+use itertools::Itertools;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    match a.iter().combinations(3).any(|v| v.into_iter().sum::<usize>() == 1000) {
+        true => println!("Yes"),
+        _ => println!("No"),
+    };
 }
